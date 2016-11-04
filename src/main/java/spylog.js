eceb4d2950bed4ecccb13320c8127a9b964448c9
Kazuhiko Arase <kazuhiko.arase@gmail.com>
2016-11-04 16:25:12 +0900

    Nashorn / Rhino compatible

--- src/main/java/spylog.js
+++ src/main/java/spylog.js
@@ -1,21 +1,42 @@
+//
+// Nashorn / Rhino compatible
+//
+if (typeof Java == 'undefined') {
+  Java = {
+      type : function(className) {
+        var path = className.split(/\./g);
+        var cls = Packages;
+        for (var i = 0; i < path.length; i += 1) {
+          cls = cls[path[i]];
+        }
+        return cls;
+      }
+  };
+}
+
 (function() {
+
+  var System = Java.type('java.lang.System');
+  var SpyDriver = Java.type('spylog.SpyDriver');
+  var Connection = Java.type('java.sql.Connection');
+  var Statement = Java.type('java.sql.Statement');
 
   var console = {
     log : function(msg) {
-      java.lang.System.out.println('[spylog]' + msg);
+      System.out.println('[spylog]' + msg);
     }
   };
 
   var handler = {
     enter : function(targetUrl, target, methodName, args) {
       var sql = null;
-      if (target instanceof java.sql.Connection) {
+      if (target instanceof Connection) {
         if ( (methodName == 'prepareStatement' ||
             methodName == 'prepareCall') &&
               args && args.length > 0) {
           sql = '' + args[0];
         }
-      } else if (target instanceof java.sql.Statement) {
+      } else if (target instanceof Statement) {
         if ( (methodName == 'execute' ||
             methodName == 'executeQuery' || 
             methodName == 'executeUpdate') &&
@@ -33,5 +54,5 @@
     exit : function(targetUrl, target, methodName) {}
   };
 
-  return new Packages.spylog.SpyDriver.Handler(handler);
+  return new SpyDriver.Handler(handler);
 }());

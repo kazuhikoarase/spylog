2ea53f09259568602a93839921f4f2acefc326a6
Kazuhiko Arase <kazuhiko.arase@gmail.com>
2017-11-06 17:58:29 +0900

    commit

--- src/main/java/spylog.js
+++ src/main/java/spylog.js
@@ -1,25 +1,30 @@
-//
 // Nashorn / Rhino compatible
-//
 if (typeof Java == 'undefined') {
-  Java = {
-    type : function(className) {
-      var path = className.split(/\./g);
-      var cls = Packages;
-      for (var i = 0; i < path.length; i += 1) {
-        cls = cls[path[i]];
+  !function(Packages) {
+    var cache = {};
+    Java = {
+      type : function(className) {
+        if (!cache[className]) {
+          var path = className.split(/\./g);
+          var cls = Packages;
+          for (var i = 0; i < path.length; i += 1) {
+            cls = cls[path[i]];
+          }
+          cache[className] = cls;
+        }
+        return cache[className];
       }
-      return cls;
-    }
-  };
+    };
+  }(Packages);
+  java = Packages = undefined;
 }
 
 (function() {
 
   var System = Java.type('java.lang.System');
-  var SpyDriver = Java.type('spylog.SpyDriver');
   var Connection = Java.type('java.sql.Connection');
   var Statement = Java.type('java.sql.Statement');
+  var SpyDriver = Java.type('spylog.SpyDriver');
 
   var console = {
     log : function(msg) {
